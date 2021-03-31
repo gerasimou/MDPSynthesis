b5d559d718d3ccd488bc9196fe38f59986610206
Simos Gerasimou <simos@cs.york.ac.uk>
2021-04-01 00:51:43 +0100

    - now MDP synthesis can run on the server

--- src/main/java/mdpSynthesis/MainMDPSynthesis.java
+++ src/main/java/mdpSynthesis/MainMDPSynthesis.java
@@ -8,17 +8,20 @@ import org.spg.PrismAPI.PrismAPI;
 import evochecker.EvoChecker;
 import evochecker.EvoCheckerMDP;
 import evochecker.auxiliary.Constants;
+import evochecker.auxiliary.Utility;
 import evochecker.plotting.PlotFactory;
 import prism.Point;
 import utilities.FileUtil;
 
 public class MainMDPSynthesis {
+
 	public static void main(String[] args) {
-		run();
+		specifyCommandLine();
+//		specifyInternal();
 	}
 	
 	
-	private static  void run() {
+	private static  void specifyInternal() {
 		 //Simple Model
 		 String problem = "simple";		
 		 //DPM Model
@@ -42,8 +45,8 @@ public class MainMDPSynthesis {
 		 //FX
 //		 problem = "fx";
 		 
-		 String model = "models/" + problem +"Old/"+ problem +"3.pm";
-		 String prop  = "models/" + problem +"Old/"+ problem +".pctl";
+		 String model = "models/" + problem +"/"+ problem +"3.pm";
+		 String prop  = "models/" + problem +"/"+ problem +".pctl";
 		 
 
 		
@@ -78,20 +81,75 @@ public class MainMDPSynthesis {
 		ec.setProperty(Constants.MAX_EVALUATIONS_KEYWORD, "200");
 		ec.setProperty(Constants.INTEGER_MUTATION_PROBABILITY, "0.9");
 		ec.setProperty("MUTATION_OPERATOR", "PolynomialUniformAll");
-
 		
+		run(ec, model, prop);
+	}
+	
+	
+	private static void specifyCommandLine() {
+		//Set configuration file
+		Utility.setPropertiesFile("config.properties");
+				
+		String model = Utility.getProperty(Constants.MODEL_FILE_KEYWORD);
+		String prop  = Utility.getProperty(Constants.PROPERTIES_FILE_KEYWORD);
+		
+		MDPSynthesis transformer = new MDPSynthesis(model, prop);
+		transformer.runEvolvables();
+
+		//1) Create EvoChecker instance
+		EvoChecker ec = new EvoCheckerMDP(transformer.internalModelRepresentation, transformer.evolvables);
+	
+		
+		String evoModel 	 = transformer.getEvoModelFile();
+		String evoProperties = transformer.getEvoPropertiesFile();
+		
+		ec.setProperty(Constants.MODEL_FILE_KEYWORD, evoModel);
+		ec.setProperty(Constants.PROPERTIES_FILE_KEYWORD, evoProperties);
+		ec.setProperty(Constants.PROCESSORS_KEYWORD, "1");
+		ec.setProperty(Constants.DOUBLE_PRECISION, "12");
+		ec.setProperty("MUTATION_OPERATOR", "PolynomialUniformAll");
+
+		run(ec, model, prop);
+	}
+		
+		
+	private static void run(EvoChecker ec, String model, String prop) {
 		//3) Start EvoChecker
 		ec.start();
 
 		//Save EvoChecker results to new file with suffix "_all"
 		String paretoFrontFilename = ec.getParetoFrontFileName();
 		String allResultsFilename  = paretoFrontFilename+"_all"; 
-		FileUtil.saveToFile(allResultsFilename, FileUtil.readFile(paretoFrontFilename), false);
-		
+
+		StringBuilder sb = new StringBuilder(FileUtil.readFile(paretoFrontFilename));
+		sb.append("\n");
+//		FileUtil.saveToFile(allResultsFilename, FileUtil.readFile(paretoFrontFilename), false);
+
 		//Add separator
-		StringBuilder sb = new StringBuilder("=\n");
+		 sb.append("=\n");
+
+		//Run Prism
+		runPrism(model, prop, sb);
+
+		//Add separator
+		 sb.append("=\n");
+
+		 //Run Storm
+		 sb.append(RunStorm.runStorm(model, prop));
 		 
-		 //Run Prism
+		 System.out.println(sb.toString());
+
+		 //Save all results to file
+		 FileUtil.saveToFile(allResultsFilename, sb.toString(), true);
+		 
+		 //Plot
+		 plot(allResultsFilename, ec.getObjectivesNum());
+	}
+	
+	
+	
+	private static void runPrism(String model, String prop, StringBuilder sb) {
+		//Run Prism
 		 PrismAPI api = new PrismAPI(null);
 		 api.parseModelAndPropertiesFiles(model, prop);
 		 api.buildModel();
@@ -110,20 +168,12 @@ public class MainMDPSynthesis {
 		 catch (Exception | Error e) {
 			 e.printStackTrace();
 		 }
-		 
-		 
-		 //Run Storm
-		 sb.append("=\n");
-		 sb.append(RunStorm.runStorm(model, prop));
-		 
-		 System.out.println(sb.toString());
-
-		 //Save Prism results to file
-		 FileUtil.saveToFile(allResultsFilename, sb.toString(), true);
-		 
-		 //Plot
+	}
+	
+	
+	private static void plot(String allResultsFilename, int objectivesNum) {
 		 try {
-			 if (ec.getObjectivesNum() == 2) {
+			 if (objectivesNum == 2) {
 				 PlotFactory.setParetoFrontScriptFile("scripts/plotFront2DComparison.py");
 				 PlotFactory.plotParetoFront(allResultsFilename, 2);
 			 }
